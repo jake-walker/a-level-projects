bdfca8a440ef02d282fc90f9efabf03eb8459157
Jacob Walker <13walkerj@chase.worcs.sch.uk>
2018-10-10 10:09:25 +0100

    Added 'Biggest Number Out Of 10' Project.

---
+++ Biggest Number Out Of 10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Biggest_Number_Out_Of_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int biggest = int.MinValue;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"Enter number {i}:");
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(" > ");
+
+                int input;
+
+                while (!int.TryParse(Console.ReadLine(), out input))
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write(" > ");
+                }
+
+                if (input >= biggest)
+                {
+                    biggest = input;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Biggest Number: {biggest}");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Press enter to quit...");
+
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter)
+            {
+                continue;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 | ---- | ---- | ----------- | ----------- |
 | Leap Year | In Lesson | Calculate if an inputted year is a leap year or not. | [Leap Year](https://github.com/jake-walker/a-level-projects/tree/master/Leap%20Year) |
 | Programming Techniques | Homework Task | Comments and code explaining 'Programming Techniques' | [Programming Techniques HW](https://github.com/jake-walker/a-level-projects/tree/master/Programming%20Techniques%20HW) |
+| Biggest Number Out Of 10 | In Lesson | Ask the user for 10 numbers and it will print the biggest number | [Biggest Number Out Of 10](https://github.com/jake-walker/a-level-projects/tree/master/Biggest%20Number%20Out%20Of%2010) |
