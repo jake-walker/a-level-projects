57a71a088dbe1e86bc9f6e0fae21be003e72dce8
Jacob Walker <13walkerj@chase.worcs.sch.uk>
2018-10-18 11:44:18 +0100

    Start of Morse Code Project

---
+++ Morse Code/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Morse_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentence to translate it to Morse Code.");
+
+            Console.Write(" > ");
+
+            string[] sentence = Console.ReadLine().Split("");
+
+            foreach (string c in sentence)
+            {
+
+            }
+        }
+    }
+}
