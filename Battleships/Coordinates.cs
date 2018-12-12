f6ad8dd6831803e303a3c1ee9f5dbd2ed28cc9db
Jacob Walker <13walkerj@chase.worcs.sch.uk>
2018-12-12 10:40:28 +0000

    Started Battleships Project

---
+++ Battleships/Coordinates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    public class Coordinates
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Coordinates(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            Char col = (Char)(97 + (Column - 1));
+            return $"{col}{Row}";
+        }
+    }
+}
