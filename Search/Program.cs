358cb8da1af5cc7e97bd16599c6b66979785d726
Jake Walker <hi@jakew.me>
2019-02-01 11:12:02 +0000

    Added Searching Algorithm Benchmark

--- Search/Program.cs
+++ Search/Program.cs
@@ -1,12 +1,94 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 
 namespace Search
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine(" 1. Manual Search");
+                Console.WriteLine(" 2. Benchmark");
+                Console.WriteLine(" 0. Quit");
+                Console.Write(" -> ");
+                var opt = Console.ReadLine().Trim();
+
+                if (opt == "1")
+                {
+                    ManualSearch();
+                } else if (opt == "2")
+                {
+                    RunBenchMark();
+                }
+                else if (opt == "0")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option! Press any key to continue...");
+                    Console.ReadKey(true);
+                }
+            }
+        }
+
+        static void RunBenchMark()
+        {
+            var benchList = new List<int>();
+            var rand = new Random();
+
+            Console.WriteLine("Generating 100,000 random numbers...");
+
+            for (var i = 0; i < 100000; i++)
+            {
+                benchList.Add(rand.Next(0, 100000));
+            }
+
+            var expected = rand.Next(benchList.Count);
+            var benchTarget = benchList[expected];
+
+            Console.WriteLine("Calculating average time of Linear Search...");
+
+            var linearWatch = Stopwatch.StartNew();
+
+            for (var i = 0; i < 1000; i++)
+            {
+                Console.Write($" - Running Linear Search {i+1} of 1000...");
+                var result = Search.Linear(benchList, benchTarget);
+                Console.WriteLine($" [DONE: R={result}]");
+            }
+
+            linearWatch.Stop();
+
+            Console.WriteLine("Calculating average time of Binary Search...");
+
+            var binaryWatch = Stopwatch.StartNew();
+
+            for (var i = 0; i < 1000; i++)
+            {
+                Console.Write($" - Running Binary Search {i+1} of 1000...");
+                var result = Search.Binary(benchList, benchTarget);
+                Console.WriteLine($" [DONE: R={result}]");
+            }
+
+            binaryWatch.Stop();
+
+            Console.WriteLine("Finished Benchmark!");
+            var linearAvg = linearWatch.ElapsedMilliseconds / 1000;
+            var binaryAvg = binaryWatch.ElapsedMilliseconds / 1000;
+            Console.WriteLine($"LINEAR Average: {linearAvg:F4} ms");
+            Console.WriteLine($"BINARY Average: {binaryAvg:F4} ms");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+
+        static void ManualSearch()
         {
             List<int> searchList = new List<int>();
             int searchItem;
